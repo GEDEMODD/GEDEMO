be2c312cb1fd96955e8cad236d863f2c867f82da
Keli <thorkell@me.com>
2016-02-14 14:31:42 +0000

    flot

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class Collector : MonoBehaviour {
+
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start(){
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 1) {
+			winText.text = "You've found all the cubes!";
+		}
+	}
+}
