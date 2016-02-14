14c088539d3382a04b17cd61f3e15851412e5085
Arnar Kari <arnarkari93@gmail.com>
2016-02-14 16:05:31 +0000

    spider

--- Assets/AIAnimal.cs
+++ Assets/AIAnimal.cs
@@ -7,17 +7,26 @@ public class AIAnimal : MonoBehaviour {
 	public int NextDest = 0;
 
 	private NavMeshAgent agent;
+	private Animation animator;
 
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent> ();
+		animator = GetComponent<Animation> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if (agent == null) { return; }
 		if (agent.remainingDistance < 0.5f) {
+			
 			agent.SetDestination (Waypoints [NextDest].position);
 			NextDest = (NextDest + 1) % Waypoints.Length;
+		}
+
+		if (agent.remainingDistance > 0.5f) {
+			animator.Play ("walk");
 		}
 	}
 }
